4f06041352a6945779f2367a2abdb2dda0cccbf0
krzysiekk22d <klas.club@gmail.com>
2023-12-22 18:06:47 +0100

    Initial Commit

---
+++ multiSelection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  multiSelection
+//
+//  Created by Krzysztof Czura on 22/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ multiSelection/multiSelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiSelectionApp.swift
+//  multiSelection
+//
+//  Created by Krzysztof Czura on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct multiSelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
